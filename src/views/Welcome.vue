37d8e14909063ab6327505d4bf332a7ef89d4a35
tatyana-ivanova <tatyana.ivanova@isobar.ru>
2021-09-13 16:24:25 +0300

    Fix welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -4,19 +4,21 @@
 	.welcome__list.flex.justify-between
 		.welcome__item
 			PointerIcon.welcome__item-icon
-			.welcome__item-title Выберите категорию
+			.welcome__item-title.flex.justify-center.items-center Выберите категорию
 			.welcome__item-content.flex.flex-wrap.justify-center
 				Button.welcome__item-product(v-for="product of products" :key="product.id"
 					type="violet-outline" @click="chooseProduct(product)"
 				) {{product.title}}
+			ArrowIcon.welcome__item-next
 		.welcome__item
 			SettingsIcon.welcome__item-icon
-			.welcome__item-title Настройте категорию
+			.welcome__item-title.flex.justify-center.items-center Настройте категорию
 			.welcome__item-text
 				| Укажите Ваши интересы в рамках категории: свои бренды, близких конкурентов для сравнения и ритейлеров.
+			ArrowIcon.welcome__item-next
 		.welcome__item
 			CalendarIcon.welcome__item-icon
-			.welcome__item-title Настройте актуальный вид дашборда, SKU брендов
+			.welcome__item-title.flex.justify-center.items-center Настройте актуальный вид дашборда, SKU брендов
 			.welcome__item-text
 				| Настройте фильтр периода статистики или данные по конкретным брендам. Еще, Вы можете скрывать виджеты и графики, которые вам не нужны и менять их порядок
 
@@ -25,10 +27,11 @@
 import PointerIcon from '@/assets/svg/pointer.svg'
 import SettingsIcon from '@/assets/svg/settings.svg'
 import CalendarIcon from '@/assets/svg/calendar.svg'
+import ArrowIcon from '@/assets/svg/arrow.svg'
 import Button from '@/components/Button/Button.vue'
 
 export default {
-	components: { PointerIcon, SettingsIcon, CalendarIcon, Button },
+	components: { PointerIcon, SettingsIcon, CalendarIcon, ArrowIcon, Button },
 	data() {
 		return {
 			products: [
@@ -64,7 +67,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 .welcome {
-	margin-top: 22px;
+	margin-top: 24px;
 	padding: 0 30px;
 
 	&__title {
@@ -74,6 +77,7 @@ export default {
 		margin-top: 60px;
 	}
 	&__item {
+		position: relative;
 		width: 32%;
 		text-align: center;
 
@@ -82,22 +86,27 @@ export default {
 			margin: 0 auto;
 		}
 		&-title {
-			margin-top: 40px;
+			height: 54px;
+			margin-top: 32px;
 			font-weight: 600;
 			font-size: 20px;
 			line-height: 1.35;
-		}
-		&-content {
-			margin-top: 18px;
 		}
 		&-product {
 			margin: 10px 10px 0 10px;
 		}
 		&-text {
-			margin-top: 32px;
+			margin-top: 14px;
 			font-size: 16px;
 			line-height: 1.75;
 			letter-spacing: 0.5px;
+		}
+		&-next {
+			position: absolute;
+			width: 46px;
+			height: 24px;
+			top: 10px;
+			right: -25px;
 		}
 	}
 }
